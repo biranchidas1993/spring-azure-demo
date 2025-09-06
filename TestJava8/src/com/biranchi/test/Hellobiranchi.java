cc695080e1aa580e4871e23df5e1408640c1d878
dasbi <dasbi@Biranchi_Das>
2025-09-06 13:27:08 +0530

    spring azure changes

---
+++ TestJava8/src/com/biranchi/test/Hellobiranchi.java
@@ -0,0 +1,11 @@
+package com.biranchi.test;
+
+public class Hellobiranchi {
+
+	public static void main(String[] args) {
+		System.out.println("hello biranchi");
+		// TODO Auto-generated method stub
+
+	}
+
+}
